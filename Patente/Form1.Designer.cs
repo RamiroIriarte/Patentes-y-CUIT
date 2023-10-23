2648b34d38693dc9233aaac54a08d5804564bb89
Ramiro Iriarte <ramiro_iriarte003@hotmail.com>
2023-10-23 11:23:54 -0300

    Se hizo la parte de CUIT

--- Patente/Form1.Designer.cs
+++ Patente/Form1.Designer.cs
@@ -30,23 +30,20 @@
         {
             this.btmCargar = new System.Windows.Forms.Button();
             this.mtxtPatente = new System.Windows.Forms.MaskedTextBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblCuit = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lblPatente = new System.Windows.Forms.Label();
+            this.rbtmCUIT = new System.Windows.Forms.RadioButton();
             this.rbtmNueva = new System.Windows.Forms.RadioButton();
             this.rbtmVieja = new System.Windows.Forms.RadioButton();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.lblPatente = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btmCargar
             // 
-            this.btmCargar.Location = new System.Drawing.Point(157, 52);
+            this.btmCargar.Location = new System.Drawing.Point(207, 71);
             this.btmCargar.Name = "btmCargar";
-            this.btmCargar.Size = new System.Drawing.Size(80, 23);
+            this.btmCargar.Size = new System.Drawing.Size(88, 23);
             this.btmCargar.TabIndex = 0;
             this.btmCargar.Text = "Cargar";
             this.btmCargar.UseVisualStyleBackColor = true;
@@ -54,68 +51,57 @@
             // 
             // mtxtPatente
             // 
-            this.mtxtPatente.Location = new System.Drawing.Point(148, 22);
+            this.mtxtPatente.Location = new System.Drawing.Point(196, 31);
             this.mtxtPatente.Name = "mtxtPatente";
-            this.mtxtPatente.Size = new System.Drawing.Size(100, 20);
+            this.mtxtPatente.Size = new System.Drawing.Size(108, 20);
             this.mtxtPatente.TabIndex = 2;
             this.mtxtPatente.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtxtPatente_MaskInputRejected);
             this.mtxtPatente.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.mtxtPatente_KeyPress);
             // 
-            // tabControl1
+            // lblCuit
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(0, 2);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(306, 177);
-            this.tabControl1.TabIndex = 3;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this.groupBox1);
-            this.tabPage1.Controls.Add(this.lblPatente);
-            this.tabPage1.Controls.Add(this.btmCargar);
-            this.tabPage1.Controls.Add(this.mtxtPatente);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(298, 151);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Patentes";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.lblCuit.AutoSize = true;
+            this.lblCuit.Location = new System.Drawing.Point(16, 152);
+            this.lblCuit.Name = "lblCuit";
+            this.lblCuit.Size = new System.Drawing.Size(0, 13);
+            this.lblCuit.TabIndex = 7;
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.rbtmCUIT);
             this.groupBox1.Controls.Add(this.rbtmNueva);
             this.groupBox1.Controls.Add(this.rbtmVieja);
             this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(116, 69);
+            this.groupBox1.Size = new System.Drawing.Size(157, 85);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Tipos de Patentes";
+            this.groupBox1.Text = "¿Que desea cargar?";
             // 
-            // lblPatente
+            // rbtmCUIT
             // 
-            this.lblPatente.AutoSize = true;
-            this.lblPatente.Location = new System.Drawing.Point(8, 105);
-            this.lblPatente.Name = "lblPatente";
-            this.lblPatente.Size = new System.Drawing.Size(0, 13);
-            this.lblPatente.TabIndex = 5;
+            this.rbtmCUIT.AutoSize = true;
+            this.rbtmCUIT.Location = new System.Drawing.Point(7, 65);
+            this.rbtmCUIT.Name = "rbtmCUIT";
+            this.rbtmCUIT.Size = new System.Drawing.Size(50, 17);
+            this.rbtmCUIT.TabIndex = 5;
+            this.rbtmCUIT.TabStop = true;
+            this.rbtmCUIT.Text = "CUIT";
+            this.rbtmCUIT.UseVisualStyleBackColor = true;
+            this.rbtmCUIT.CheckedChanged += new System.EventHandler(this.rbtmCUIT_CheckedChanged);
             // 
             // rbtmNueva
             // 
             this.rbtmNueva.AutoSize = true;
             this.rbtmNueva.Location = new System.Drawing.Point(7, 19);
             this.rbtmNueva.Name = "rbtmNueva";
-            this.rbtmNueva.Size = new System.Drawing.Size(57, 17);
+            this.rbtmNueva.Size = new System.Drawing.Size(97, 17);
             this.rbtmNueva.TabIndex = 4;
             this.rbtmNueva.TabStop = true;
-            this.rbtmNueva.Text = "Nueva";
+            this.rbtmNueva.Text = "Patente Nueva";
             this.rbtmNueva.UseVisualStyleBackColor = true;
             this.rbtmNueva.CheckedChanged += new System.EventHandler(this.rbtmNueva_CheckedChanged);
             // 
@@ -124,37 +110,37 @@
             this.rbtmVieja.AutoSize = true;
             this.rbtmVieja.Location = new System.Drawing.Point(7, 42);
             this.rbtmVieja.Name = "rbtmVieja";
-            this.rbtmVieja.Size = new System.Drawing.Size(48, 17);
+            this.rbtmVieja.Size = new System.Drawing.Size(88, 17);
             this.rbtmVieja.TabIndex = 3;
             this.rbtmVieja.TabStop = true;
-            this.rbtmVieja.Text = "Vieja";
+            this.rbtmVieja.Text = "Patente Vieja";
             this.rbtmVieja.UseVisualStyleBackColor = true;
             this.rbtmVieja.CheckedChanged += new System.EventHandler(this.rbtmVieja_CheckedChanged);
             // 
-            // tabPage2
+            // lblPatente
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(330, 141);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "CUIT";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.lblPatente.AutoSize = true;
+            this.lblPatente.Location = new System.Drawing.Point(16, 122);
+            this.lblPatente.Name = "lblPatente";
+            this.lblPatente.Size = new System.Drawing.Size(0, 13);
+            this.lblPatente.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(445, 191);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(357, 172);
+            this.Controls.Add(this.lblCuit);
+            this.Controls.Add(this.lblPatente);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btmCargar);
+            this.Controls.Add(this.mtxtPatente);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -162,13 +148,12 @@
 
         private System.Windows.Forms.Button btmCargar;
         private System.Windows.Forms.MaskedTextBox mtxtPatente;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.RadioButton rbtmNueva;
         private System.Windows.Forms.RadioButton rbtmVieja;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Label lblPatente;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtmCUIT;
+        private System.Windows.Forms.Label lblCuit;
     }
 }
 
--- Patente/Form1.cs
+++ Patente/Form1.cs
@@ -24,24 +24,42 @@ namespace Patente
 
         private void btmCargar_Click(object sender, EventArgs e)
         {
-             
-                if (mtxtPatente.Text != "")
+            if (rbtmVieja.Checked == true || rbtmNueva.Checked == true)
+            {
+                if (mtxtPatente.Text.Length != 6 && mtxtPatente.Text.Length != 7)
                 {
-                    lblPatente.Text = "La patente cargada es:" + mtxtPatente.Text;
+                    MessageBox.Show("Debe llenar todos los espacios en blanco");
                 }
                 else
                 {
-                    MessageBox.Show("Debe llenar los espacios en blanco");
+                    rbtmVieja.Checked = false;
+                    rbtmNueva.Checked = false;
+                    lblPatente.Text = "La patente cargada es: " + mtxtPatente.Text.ToUpper();
+                    mtxtPatente.Text = "";
                 }
+            }
+            else
+            {
+                if (mtxtPatente.Text.Length != 11)
+                {
+                    MessageBox.Show("Debe llenar todos los espacios en blanco");
+                }
+                else
+                {
+                    lblPatente.Text = "El CUIT pertenece al DNI: " + mtxtPatente.Text.Substring(2, 8);
+                    lblCuit.Text = "EL CUIT cargado es: " + mtxtPatente.Text.Substring(0, 2) + "-" + mtxtPatente.Text.Substring(2, 8) + "-" + mtxtPatente.Text.Substring(10, 1);
+                    mtxtPatente.Text = "";
+                }
+            }
           
-            
+                   
         }
 
         private void rbtmVieja_CheckedChanged(object sender, EventArgs e)
         {
             if (rbtmVieja.Checked)
             {
-                mtxtPatente.Mask = " LLL000";
+                mtxtPatente.Mask = "LLL000";
                
             }
         }
@@ -57,18 +75,28 @@ namespace Patente
 
         private void mtxtPatente_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
-            MessageBox.Show("Error al introducir el dato");
+            lblPatente.Text = "Error al introducir el dato";
         }
 
         private void mtxtPatente_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (rbtmVieja.Checked == false && rbtmNueva.Checked == false)
+            if (rbtmVieja.Checked == false && rbtmNueva.Checked == false && rbtmCUIT.Checked == false)
             {
+                e.Handled = true;
                 MessageBox.Show("Seleccione un tipo de patente");
-
-                mtxtPatente.Text = "";
             }
             
         }
+
+        private void rbtmCUIT_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtmCUIT.Checked)
+            {
+                mtxtPatente.Mask = "00000000000";
+
+            }
+        }
+
+
     }
 }
